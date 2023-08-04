2d8b6731ed6e1a6aabbd117e818c9d092c70cbc3
CJReynoso <140649611+CJReynoso@users.noreply.github.com>
2023-08-04 13:05:04 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,37 @@
+// eslint-disable-next-line
 import React, {useContext, userContext} from 'react';
 import {AppContext} from '../context/AppContext'
 
 const Budget = () =>
 {
     const {budget} = useContext(AppContext);
+    const {expenses} = useContext(AppContext);
+    const maxBudget=20000;
+
+    const onChangeBudget = event =>
+    {
+        const totalExpenses = expenses.reduce((total, item) =>
+        {
+            return (total = total + item.cost);
+        }, 0);
+
+        if(event.target.value > maxBudget) 
+            {
+                alert("The value cannot exceed "+ maxBudget);
+                return;
+            }
+
+            if(event.target.value < totalExpenses) 
+            {
+                alert("The value cannot be less than the spending amount ("+ totalExpenses +")");
+                return;
+            }
+    }
 
     return(
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input type='number' step='10' min='0' placeholder={budget} onChange={onChangeBudget}></input>
         </div>
     );
 }//end Budget
