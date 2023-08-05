da8500d77c577906ada14646abfc2e835a33f683
CJReynoso <140649611+CJReynoso@users.noreply.github.com>
2023-08-04 18:33:32 -0600

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useContext} from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Currency = () =>
 {
+    const { dispatch} = useContext(AppContext);
+
+    const changeCurrency= (currency) =>
+    {
+        dispatch(
+            {
+                type: 'CHG_CURRENCY',
+                payload: currency,
+            });
+
+    }//end changeCurrency
+
     return(
         <div className='alert alert-secondary' style={{ backgroundColor: 'blue', color: 'white'}}>
             <span>Currency: </span>
-            <select style={{backgroundColor: 'lightblue'}}name="Currency" id="Cu">
-            <option value="Pnd">£ Pound</option>
-            <option value="Dlls">$ Dollar</option>
-            <option value="Eur">€ Euro</option>
-            <option value="Ru">₹ Ruppee</option>
+            <select style={{backgroundColor: 'lightblue'}}name="Currency" id="currency" onChange={event=>changeCurrency(event.target.value)}>
+            <option value="£">£ Pound</option>
+            <option value="$">$ Dollar</option>
+            <option value="€">€ Euro</option>
+            <option value="₹">₹ Ruppee</option>
             </select>
         </div>
     );
